b0124f438bf0c9183d7a427cd0d1a055065b2856
lep <lep.delete@googlemail.com>
2022-08-22 16:25:40 +0200

    Provide nix flake
    I'm rather new to nix and flakes so there is probably plenty to improve.

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+    inputs = {
+	nixpkgs.url = "github:NixOS/nixpkgs";
+	flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+	flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems ++ [flake-utils.lib.system.aarch64-darwin]) (system:
+	    let pkgs = nixpkgs.legacyPackages.${system};
+		packageName = "jassdoc";
+		mkdocs = pkgs.haskellPackages.callCabal2nix "jassdoc" self rec {
+		    # Dependency overrides go here
+		};
+		buildInputs = [
+		    pkgs.gnumake
+		    pkgs.perl
+		    pkgs.sqlite
+		    mkdocs
+		];
+	    in rec {
+		packages = {
+		    ${packageName} = pkgs.stdenv.mkDerivation {
+			name = "jassdoc";
+			src = self;
+			inherit buildInputs;
+
+			buildPhase = "MKDOCS=${mkdocs}/bin/mkdocs make jass.db";
+			installPhase = ''
+			    mkdir -p $out
+			    install -t $out jass.db
+			'';
+		    };
+		};
+
+		defaultPackage = packages.${packageName};
+
+		devShell = pkgs.mkShell {
+		    buildInputs = buildInputs ++ [ pkgs.cabal-install ];
+		    inputsFrom = builtins.attrValues self.packages.${system};
+		};
+	    });
+}
+
