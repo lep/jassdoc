c394160bb0dc3cda6f5b05da37bd4ca016f43d7c
lep <lep.delete@googlemail.com>
2023-09-10 10:57:50 +0200

    Reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -1,44 +1,37 @@
 {
-    inputs = {
-	nixpkgs.url = "github:NixOS/nixpkgs";
-	flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, flake-utils }:
-	flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems ++ [flake-utils.lib.system.aarch64-darwin]) (system:
-	    let pkgs = import nixpkgs { inherit system; };
-		packageName = "jassdoc";
-		mkdocs = pkgs.haskellPackages.callCabal2nix packageName self rec {
-		    # Dependency overrides go here
-		};
-		buildInputs = [
-		    pkgs.gnumake
-		    pkgs.perl
-		    pkgs.sqlite
-		    mkdocs
-		];
-	    in rec {
-		packages = {
-		    ${packageName} = pkgs.stdenv.mkDerivation {
-			name = "jassdoc";
-			src = self;
-			inherit buildInputs;
+  outputs = { self, nixpkgs, flake-utils, }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          packageName = "jassdoc";
+          mkdocs = pkgs.haskellPackages.callCabal2nix packageName self {
+            # Dependency overrides go here
+          };
+          buildInputs = [ pkgs.gnumake pkgs.perl pkgs.sqlite mkdocs ];
+        in rec {
+          packages = {
+            ${packageName} = pkgs.stdenv.mkDerivation {
+              name = "jassdoc";
+              src = self;
+              inherit buildInputs;
 
-			buildPhase = "MKDOCS=${mkdocs}/bin/mkdocs make jass.db";
-			installPhase = ''
-			    mkdir -p $out
-			    install -t $out jass.db
-			'';
-		    };
-		};
+              buildPhase = "MKDOCS=${mkdocs}/bin/mkdocs make jass.db";
+              installPhase = ''
+                install -Dt $out jass.db
+              '';
+            };
+          };
 
-		defaultPackage = packages.${packageName};
+          defaultPackage = packages.${packageName};
 
-		devShell = pkgs.mkShell {
-		    buildInputs = buildInputs ++ [ pkgs.cabal-install ];
-		    inputsFrom = builtins.attrValues self.packages.${system};
-				packages = [ pkgs.perlPackages.DBI pkgs.perlPackages.DBDSQLite ];
-		};
-	    });
+          devShell = pkgs.mkShell {
+            buildInputs = buildInputs ++ [ pkgs.cabal-install ];
+            inputsFrom = builtins.attrValues self.packages.${system};
+            packages = [ pkgs.perlPackages.DBI pkgs.perlPackages.DBDSQLite ];
+          };
+        });
 }
-
