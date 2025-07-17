dec658f8715d3b60def60c0f2940b6d05745dede
Luashine <103937213+Luashine@users.noreply.github.com>
2025-07-17 02:18:37 +0200

    builtin.lua: explain TypeDefine

--- extra-lua/builtin.lua
+++ extra-lua/builtin.lua
@@ -1,1 +1,11 @@
--- function TypeDefine // , lua C closure
+---Defines a new subtype using the game API.
+---
+---This is used to replace the Jass syntax: `type agent extends handle`
+---with: `TypeDefine('agent', 'handle')`
+---
+---In contrast, natives do not need to be registered in Lua. They are exported automatically.
+---@param newType string
+---@param parentType string
+function TypeDefine(newType, parentType)
+	-- function TypeDefine is a lua C closure
+end
