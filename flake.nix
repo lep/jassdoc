e23d61190a55f967c62d8c95f82c9764788a01b1
lep <lep.delete@googlemail.com>
2025-08-01 15:15:56 +0200

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
   };
 
